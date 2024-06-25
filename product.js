29e3fd937438415d83e6f3dc036b3528a74bc385
Fazilmd152 <mohamedfazil152@gmail.com>
2024-06-25 11:01:08 +0530

    Search functions added

--- product.js
+++ product.js
@@ -8,3 +8,25 @@ menu.addEventListener("click",function(){
 xmark.addEventListener("click",function(){
     sidenav.style.right='-50%'
 })
+
+// search function
+var productcontainer=document.getElementById("productcontainer")
+var search=document.getElementById("search")
+var productlist=productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredvalue=event.target.value.toUpperCase()
+    
+    for(count=0;count<productlist.length;count++)
+        {
+            var productname=productlist[count].querySelector("h1").textContent
+            if(productname.toUpperCase().indexOf(enteredvalue)<0)
+                {
+                    productlist[count].style.display="none"
+                }
+                else{
+                    productlist[count].style.display="block"
+                }
+        }
+})
+
